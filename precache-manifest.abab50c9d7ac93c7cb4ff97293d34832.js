80cbc417e5c5cbd14650045d2abe4240ddbdd64c
Fabien Lesage <fabienlesage@fabiens-macbook-pro.home>
2019-11-09 17:23:11 +0100

    Updates

---
+++ precache-manifest.abab50c9d7ac93c7cb4ff97293d34832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c4c0851e8baf23b35cd670e54e12f6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8705720ceb83136214b2",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "026e0e5bd3a3c042ec22",
+    "url": "/resume/static/js/2.d7c79c63.chunk.js"
+  },
+  {
+    "revision": "8705720ceb83136214b2",
+    "url": "/resume/static/js/main.6a1f6f53.chunk.js"
+  },
+  {
+    "revision": "743c68d407c94b674a46",
+    "url": "/resume/static/js/runtime-main.0fcc9084.js"
+  }
+]);
